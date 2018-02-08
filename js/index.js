2f535f69af1aed803f3dda80a1b1c56d89b04cfe
itsjtwright <itsjtwright@gmail.com>
2018-02-07 22:21:37 -0700

    Cleaned up

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
-$(document).ready(function() {
+$(document).ready(function () {
   setImage(".w1a000", "http://k003.kiwi6.com/hotlink/2wgaui3q9b/IMG_2168.mp3");
   setImage(".w1a002", "http://k003.kiwi6.com/hotlink/lnfw0n0xuv/Trueaudio.mp3");
   setImage(".w1a001", "http://k003.kiwi6.com/hotlink/cmngfubqa7/vidw1a001.mp3");
   setImage(".w2a000", "http://k003.kiwi6.com/hotlink/xa4le6zyyw/w2a000.mp3");
   setImage('.w2a003', "");
-  setImage(".w2a001","");
-  setImage(".w2a002","")
-  setImage(".w3a000","http://k003.kiwi6.com/hotlink/o8yygegbtg/IMG_2195.mp3")
-  setImage(".w4a000","http://k003.kiwi6.com/hotlink/acx55wsq3d/IMG_2226.mp3")
-  setImage(".w4a002","")
+  setImage(".w2a001", "");
+  setImage(".w2a002", "")
+  setImage(".w3a000", "http://k003.kiwi6.com/hotlink/o8yygegbtg/IMG_2195.mp3")
+  setImage(".w4a000", "http://k003.kiwi6.com/hotlink/acx55wsq3d/IMG_2226.mp3")
+  setImage(".w4a002", "")
 });
 function setImage(image, sound) {
   let thisAudio = new Audio(sound);
   if ($(window).width() < $(window).height())
-      // || window.matchMedia("only screen and (max-width: 760px)"))
-      {
+  // || window.matchMedia("only screen and (max-width: 760px)"))
+  {
 
-    $(image).css({ "width": "100%","display":"block","position":"relative" })
-      $(image).on("click", function() {
+    $(image).css({ "width": "100%", "display": "block", "position": "relative" })
+    $(image).on("click", function () {
       thisAudio.play();
     }).before(image.substr(1));
-    $("body").css({"overflow":"scroll"});
+    $("body").css({ "overflow": "scroll" });
   } else {
     $(image)
       .css({
@@ -29,7 +29,7 @@ function setImage(image, sound) {
         left: Math.floor(Math.random() * 90) + "vw",
         top: Math.floor(Math.random() * 90) + "vh"
       })
-      .mouseenter(function() {
+      .mouseenter(function () {
         $(".data").text(image.substr(1));
         thisAudio.play();
         $(image).css({
@@ -38,7 +38,7 @@ function setImage(image, sound) {
           "z-index": 100
         });
       })
-      .on("mouseleave",function() {
+      .on("mouseleave", function () {
         $(".data").empty();
         thisAudio.pause();
         $(image).css({
