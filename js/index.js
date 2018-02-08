1b67caf981d4d2149ac977a4b2c22dd26ebfbb0d
itsjtwright <itsjtwright@gmail.com>
2018-02-07 23:31:08 -0700

    implemented clean mode

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+  setImages();
+  $(".cleanCheck").change(function() {
+    $('.entry').remove();
+    $('.p').attr("style","").off();
+    setImages();
+  });
+});
+
+function setImages() {
   setImage(".w1a000", "http://k003.kiwi6.com/hotlink/2wgaui3q9b/IMG_2168.mp3");
   setImage(".w1a002", "http://k003.kiwi6.com/hotlink/lnfw0n0xuv/Trueaudio.mp3");
   setImage(".w1a001", "http://k003.kiwi6.com/hotlink/cmngfubqa7/vidw1a001.mp3");
@@ -10,26 +19,33 @@ $(document).ready(function () {
   setImage(".w4a000","http://k003.kiwi6.com/hotlink/acx55wsq3d/IMG_2226.mp3")
   setImage(".w5a000","");
   setImage(".w4a002","");
-});
+}
+
 function setImage(image, sound) {
   let thisAudio = new Audio(sound);
   if ($(window).width() < $(window).height())
   // || window.matchMedia("only screen and (max-width: 760px)"))
   {
-
     $(image).css({ "width": "100%", "display": "block", "position": "relative" })
     $(image).on("click", function () {
       thisAudio.play();
     }).before(image.substr(1));
-    $("body").css({ "overflow": "scroll" });
+    $("body").css({ "overflow": "auto" });
   } else if(document.getElementById("cleanCheck").checked) {
-    $(image).css({ "width": "25%", "display": "inline-block", "position": "relative" })
+    $('.imageWrapper').css("display:flex");
+    $(image).css({
+      transform: "rotate(0deg)",
+      "z-index": 100
+    });
+    $(image).css({ "position":"relative","width":"33%" }).mouseenter(function () {
+     document.title = image.substr(1);
+    }).mouseleave(function(){document.title = "theworkofus"});
     $(image).on("click", function () {
       thisAudio.play();
-    }).before(image.substr(1));
-    $("body").css({ "overflow": "scroll" });
-  }
-  else {
+    })
+    $("body").css({ "overflow": "auto" });
+  } else {
+    $("body").css({ "overflow": "hidden" });
     $(image)
       .css({
         width: Math.floor(Math.random() * 500),
@@ -41,7 +57,7 @@ function setImage(image, sound) {
         $(".data").text(image.substr(1));
         thisAudio.play();
         $(image).css({
-          width: "100vh",
+          width: "30vw",
           transform: "rotate(0deg)",
           "z-index": 100
         });
