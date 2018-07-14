386187230e5cef6b1b3f7278d284103eceb55dee
JT Wright <itsjtwright@gmail.com>
2018-07-14 16:58:29 -0600

    A new version of the site, version 1

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function setImage(image) {
+    document.getElementById('image-box').src = image;
+    document.getElementById('image-box').style.border = "2px solid #2f3436";
+    document.getElementById('image-box').style.boxShadow = "5px 5px 5px black";
+}
