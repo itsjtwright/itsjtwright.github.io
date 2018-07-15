bcd5513083023f465ea7c9e21e29aa915ad4f902
JT Wright <itsjtwright@gmail.com>
2018-07-14 22:51:35 -0600

    added first week pics

--- js/theworkofus.js
+++ js/theworkofus.js
@@ -8,19 +8,8 @@ $(document).ready(function() {
 });
 
 function setImages() {
-    setImage(".w1a000", "http://k003.kiwi6.com/hotlink/2wgaui3q9b/IMG_2168.mp3");
-    setImage(".w1a002", "http://k003.kiwi6.com/hotlink/lnfw0n0xuv/Trueaudio.mp3");
-    setImage(".w1a001", "http://k003.kiwi6.com/hotlink/cmngfubqa7/vidw1a001.mp3");
-    setImage(".w2a000", "http://k003.kiwi6.com/hotlink/xa4le6zyyw/w2a000.mp3");
-    setImage('.w2a003', "");
-    setImage(".w2a001", "");
-    setImage(".w2a002", "")
-    setImage(".w3a000", "http://k003.kiwi6.com/hotlink/o8yygegbtg/IMG_2195.mp3")
-    setImage(".w4a000", "http://k003.kiwi6.com/hotlink/acx55wsq3d/IMG_2226.mp3")
-    setImage(".w5a000", "");
-    setImage(".w4a002", "");
-    setImage(".w6a000", "http://k003.kiwi6.com/hotlink/nlryes3mos/IMG_2262.mp3");
-    setImage(".w7a000", "http://k003.kiwi6.com/hotlink/04zvy6usr1/IMG_2274.mp3")
+    setImage(".w0a000", "http://k003.kiwi6.com/hotlink/mz4rf4o9it/w0a000.m4a");
+    setImage(".w0a001", "http://k003.kiwi6.com/hotlink/q29617xpe2/w0a001.m4a");
 }
 
 function setImage(image, sound) {
