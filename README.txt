bb603b02b89a445e245a2fe5a03aff35d8551382
JT Wright <jtwright@knox.edu>
2018-11-19 22:45:56 -0600

    minimal but it works

--- README.txt
+++
@@ -1,3 +0,0 @@
-This is a place for me to store little web projects as well as my portfolio until further notice.
-
-Thanks for stopping by :)
--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-function setImage(image, url) {
-    document.getElementById('image-box').src = image;
-    document.getElementById('image-box').style.border = "2px solid #2f3436";
-    document.getElementById('image-box').style.boxShadow = "5px 5px 5px black";
-    document.getElementById('image-a-tag').href = url;
-}
