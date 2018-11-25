bda651861c0b82f52200e14789443f355f425171
JT Wright <jtwright@knox.edu>
2018-11-25 16:09:59 -0600

    its simple, it works. I think that is what matters

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
 let app = new Vue({
-    el: '#main',
+    el: '#app',
     data: {
-        workArr : [{title:"HERENOW",desc:"A place for college students to show off what they have created while critiquing, indulging, tipping and purchasing the works of others."}]
-    },
-    methods: {
-        workDisplay: function() {
-            let outer = document.getElementById("outerCircle");
-            let display = document.getElementById("displayOuterCircle");
-            let projs = document.getElementById("projects");
-            outer.style.display = "none";
-            display.style.display = "flex";
-        },
-        displayDetails: function() {
-            console.log(this.$data.workArr[0].desc);
-        }
+      projects: [
+        {title:"Wrong Design Apparel",
+        src:"assets/wrongDesign.png"}
+      ]
     }
   })
