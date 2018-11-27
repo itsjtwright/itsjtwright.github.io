16c95eed204d7e7f8bc1742724afdbabf87797e9
JT Wright <jtwright@knox.edu>
2018-11-27 15:57:59 -0600

    adding to the apparel section

---
+++ apparel/cop/01/script.js
@@ -0,0 +1,28 @@
+let flip = 0;
+let margin = 0;
+
+let app = new Vue({
+    el: '#app',
+    data: {
+      projects: [
+        {id:"01",title:"Shirt Design",
+        src:"noCoastBlues.png"},
+        {id:"02",title:"Shirt Design",
+        src:"noCoastBlues.png"}
+      ]
+    },
+    methods: {
+      setMargin: function() {
+        if(flip%3===2) {
+          margin = margin + 100;
+          console.log("if"+flip)
+          flip++;
+          return margin + "px";
+        } else {
+          console.log("else"+flip)
+          flip++;
+          return margin + "px"
+        }
+      }
+    }
+  })
--- apparel/script.js
+++ apparel/script.js
@@ -2,9 +2,9 @@ let app = new Vue({
     el: '#app',
     data: {
       projects: [
-        {id:"01",title:"Wrong Design Apparel type with warped geometric form",
+        {id:"01",title:"No Coast Blues warped italicization",
         page:"cop/01/"},
-        {id:"02",title:"No Coast Blues warped italicization",
+        {id:"02",title:"Wrong Design Apparel type with warped geometric form",
         page:"cop/02/"}
       ]
     }
