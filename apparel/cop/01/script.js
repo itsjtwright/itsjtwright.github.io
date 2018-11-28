8222ac03db0337b2f302729dfd78414e35c0e797
JT Wright <jtwright@knox.edu>
2018-11-28 16:04:07 -0600

    desc works on mobile now

--- apparel/cop/01/script.js
+++ apparel/cop/01/script.js
@@ -3,29 +3,35 @@ let margin = 0;
 
 let app = new Vue({
     el: '#app',
-    data: {
+    data: function() {
+      return {
       projects: [
-        {id:"01",title:"Shirt Design",
+        {id:0,title:"Shirt Design",
         src:"noCoastBlues.png"},
-        {id:"02",title:"Shirt Design",
-        src:"noCoastBlues.png",price:"$18"}
-      ]
+        {id:1,title:"Shirt Design",
+        src:"noCoastBlues.png",price:"$18",desc:"Art of the shirt, dummy item"}
+      ],
+      showDesc: false
+    }
     },
     methods: {
       setMargin: function() {
         if(flip%3===2) {
           margin = margin + 100;
-          console.log("if"+flip)
           flip++;
           return margin + "px";
         } else {
-          console.log("else"+flip)
           flip++;
           return margin + "px"
         }
       },
-      showDetails: function() {
-        
+      showDetails: function(id) {
+        margin=0;
+        flip=0;
+        console.log(id);
+        this.showDesc = true;
+        document.getElementById("descTitle").innerHTML = this.projects[id].title;
+        document.getElementById("rest").innerHTML = this.projects[id].desc;
       }
     }
   })
